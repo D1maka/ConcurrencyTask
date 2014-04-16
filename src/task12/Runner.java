153b0b2aae09bea84116630e854ef647b6f8277a
D1maka <veres.dima@gmail.com>
2014-04-16 12:16:35 +0300

    Sin task2 fixed

--- src/task12/Runner.java
+++ src/task12/Runner.java
@@ -10,13 +10,14 @@ public class Runner {
 	public static final int N = 200;
 
 	public static void main(String[] args) {
+		double sum = 0;
 		long startTime = Calendar.getInstance().getTimeInMillis();
 		int segment = (2 * N + 1) / THREADS_NUMBER;
 		int startValue = -N;
 		int additional = (2 * N) % THREADS_NUMBER;
 		
 		int endValue = startValue + segment;
-		ArrayList<Thread> threads = new ArrayList<Thread>();
+		ArrayList<SinCalculator> threads = new ArrayList<SinCalculator>();
 
 		for (int i = 0; i < THREADS_NUMBER; i++) {
 			threads.add(new task12.SinCalculator(startValue, endValue));
@@ -41,9 +42,12 @@ public class Runner {
 				e.printStackTrace();
 			}
 		}
+		
+		for (int i = 0; i < threads.size(); i++) {
+			sum += threads.get(i).getResult();
+		}
 		startTime = Calendar.getInstance().getTimeInMillis() - startTime;
-		System.out.println("Sin value: " + task12.SinCalculator.sinValue);
-		System.out.println("Executions: " + task12.SinCalculator.executions);
+		System.out.println("Sin value: " + sum);
 		System.out.println("Algo time: " + startTime);
 	}
 
--- src/task12/SinCalculator.java
+++ src/task12/SinCalculator.java
@@ -2,10 +2,7 @@ package task12;
 
 public class SinCalculator extends Thread {
 
-	public static volatile Thread currentThread;
-	public static volatile boolean controlTaken = false;
-	public static volatile double sinValue;
-	public static volatile int executions;
+	private double result;
 
 	private int startValue;
 	private int endValue;
@@ -17,25 +14,12 @@ public class SinCalculator extends Thread {
 
 	@Override
 	public void run() {
-		Double result = new Double(0);
 		for (int i = startValue; i <= endValue; i++) {
 			result += Math.sin(i);
 		}
-		while (controlTaken) {
-			try {
-				currentThread.join();
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		} 
-		
-		controlTaken = true;
-		currentThread = this;
-		sinValue += result;
-		executions++;
-
-		controlTaken = false;
 	}
 
+	public double getResult() {
+		return result;
+	}
 }
